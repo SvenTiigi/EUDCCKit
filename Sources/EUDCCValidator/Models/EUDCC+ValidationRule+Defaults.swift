8de4d931e9efe4e3ac25d2a59bb008dd73c0c19d
Sven Tiigi <sven.tiigi@gmail.com>
2021-07-07 00:07:32 +0200

    Improved default ValidationRules

--- Sources/EUDCCValidator/Models/EUDCC+ValidationRule+Defaults.swift
+++ Sources/EUDCCValidator/Models/EUDCC+ValidationRule+Defaults.swift
@@ -1,7 +1,7 @@
 import EUDCC
 import Foundation
 
-// MARK: - EUDCC Content ValidationRule
+// MARK: - EUDCC Content Type
 
 public extension EUDCC.ValidationRule {
     
@@ -37,18 +37,79 @@ public extension EUDCC.ValidationRule {
     
 }
 
-// MARK: - EUDCC is Vaccination complete
+// MARK: - EUDCC Vaccination
 
 public extension EUDCC.ValidationRule {
     
     /// Has received all vaccination doses
     static var isVaccinationComplete: Self {
-        isVaccination && .compare(
-            value: \.vaccination?.doseNumber,
-            to: .keyPath(\.vaccination?.totalSeriesOfDoses),
-            operator: ==,
-            tag: "vaccinationHasAllDoses"
-        )
+        .isVaccination
+            && .compare(
+                value: \.vaccination?.doseNumber,
+                to: .keyPath(\.vaccination?.totalSeriesOfDoses),
+                operator: ==,
+                tag: "isVaccinationComplete"
+            )
+    }
+    
+    /// Vaccination can be considered as fully immunized
+    /// - Parameters:
+    ///   - minimumDaysPast: The amount of minimum days past since the last vaccination. Default value `15`
+    ///   - calendar: The Calendar that should be used. Default value `.current`
+    static func isFullyImmunized(
+        minimumDaysPast: Int = 15,
+        using calendar: Calendar = .current
+    ) -> Self {
+        .isVaccinationComplete
+            && .init(
+                tag: "isFullyImmunized-after-\(minimumDaysPast)-days"
+            ) { eudcc in
+                // Verify Vaccination is available
+                guard let vaccination = eudcc.vaccination else {
+                    // Otherwise return false
+                    return false
+                }
+                // Verify fully immunized Date is available from calendar
+                guard let fullyImmunizedDate = calendar.date(
+                    byAdding: .day,
+                    value: minimumDaysPast,
+                    to: vaccination.dateOfVaccination
+                ) else {
+                    // Otherwise return false
+                    return false
+                }
+                // Current Date must be greater than the fully immunized Date
+                return Date() > fullyImmunizedDate
+            }
+    }
+    
+}
+
+
+// MARK: - EUDCC Test
+
+public extension EUDCC.ValidationRule {
+    
+    /// TestResult of Test is positive
+    static var isTestedPositive: Self {
+        .isTest
+            && .compare(
+                value: \.test?.testResult.value,
+                to: .constant(EUDCC.Test.TestResult.WellKnownValue.positive.rawValue),
+                operator: ==,
+                tag: "isTestedPositive"
+            )
+    }
+    
+    /// TestResult of Test is negative
+    static var isTestedNegative: Self {
+        .isTest
+            && .compare(
+                value: \.test?.testResult.value,
+                to: .constant(EUDCC.Test.TestResult.WellKnownValue.negative.rawValue),
+                operator: ==,
+                tag: "isTestedNegative"
+            )
     }
     
 }
