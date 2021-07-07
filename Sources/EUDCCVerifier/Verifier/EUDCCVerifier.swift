3174cf8b387549d008d41052949618220edb5337
Sven Tiigi <sven.tiigi@gmail.com>
2021-07-07 17:04:56 +0200

    Refactored EUDCCVerifier from class to struct

--- Sources/EUDCCVerifier/Verifier/EUDCCVerifier.swift
+++ Sources/EUDCCVerifier/Verifier/EUDCCVerifier.swift
@@ -4,7 +4,7 @@ import Foundation
 // MARK: - EUDCCVerifier
 
 /// An EUDCC Verifier
-public final class EUDCCVerifier {
+public struct EUDCCVerifier {
     
     // MARK: Properties
     
@@ -67,13 +67,6 @@ public extension EUDCCVerifier {
         eudcc: EUDCC,
         completion: @escaping (VerificationResult) -> Void
     ) {
-        // Verify EUDCC KeyID is available from CryptographicSignature
-        guard let eudccKeyID = EUDCC.TrustCertificate.KeyID(
-            cryptographicSignature: eudcc.cryptographicSignature
-        ) else {
-            // Otherwise complete with failure
-            return completion(.failure(.malformedCertificateKeyID(eudcc)))
-        }
         // Retrieve Certificates from TrustService
         self.trustService.getCertificates { result in
             /// Complete with VerificationResult
@@ -93,45 +86,72 @@ public extension EUDCCVerifier {
                     }
                 }
             }
-            // Declare TrustCertificates
-            let trustCertificates: [EUDCC.TrustCertificate]
             // Switch on Result
             switch result {
-            case .success(let certificates):
-                // Initialize TrustCertificates
-                trustCertificates = certificates
+            case .success(let trustCertificates):
+                // Verify EUDCC against TrustCertificates
+                let verificationResult = self.verify(
+                    eudcc: eudcc,
+                    against: trustCertificates
+                )
+                // Complete with VerificationResult
+                complete(with: verificationResult)
             case .failure(let error):
                 // Complete with failure
-                return complete(with: .failure(.trustServiceError(error)))
+                complete(with: .failure(.trustServiceError(error)))
             }
-            // Retrieve matching TrustCertificates by KeyID
-            let matchingTrustCertificates = trustCertificates.filter { $0.keyID == eudccKeyID }
-            // Verify matching TrustCertificates are not empty
-            guard !matchingTrustCertificates.isEmpty else {
-                // Otherwise complete with failure
-                return complete(with: .failure(.noMatchingTrustCertificate(eudccKeyID)))
-            }
-            // Retrieve EUDCC Signature bytes
-            let eudccSignature = Data(eudcc.cryptographicSignature.signature)
-            // Initialize EUDCC SignedPayload
-            let eudccSignedPayload = EUDCC.SignedPayload(cryptographicSignature: eudcc.cryptographicSignature)
-            // Map matching SignerCertificates to VerificationCandidates
-            let verificationCandidates: [EUDCC.VerificationCandidate] = matchingTrustCertificates
-                .map { trustCertificate in
-                    .init(
-                        signature: eudccSignature,
-                        signedPayload: eudccSignedPayload,
-                        trustCertificate: trustCertificate
-                    )
-                }
-            // Verify the first VerificationCandidate that is verified successfully is available
-            guard let matchingVerificationCandidate = verificationCandidates.first(where: { $0.verify() }) else {
-                // Otherwise complete with failure as no VerificationCandidate verified successfully
-                return complete(with: .invalid)
-            }
-            // Complete with success
-            return complete(with: .success(matchingVerificationCandidate.trustCertificate))
         }
+    }
+    
+}
+
+// MARK: - Verify against TrustCertificates
+
+public extension EUDCCVerifier {
+    
+    /// Verify EUDCC against a given sequence of EUDCC TrustCertificates
+    /// - Parameters:
+    ///   - eudcc: The EUDCC
+    ///   - trustCertificates: The sequence of EUDCC TrustCertificate
+    /// - Returns: A VerificationResult
+    func verify<Certificates: Sequence>(
+        eudcc: EUDCC,
+        against trustCertificates: Certificates
+    ) -> VerificationResult where Certificates.Element == EUDCC.TrustCertificate {
+        // Verify EUDCC KeyID is available from CryptographicSignature
+        guard let eudccKeyID = EUDCC.TrustCertificate.KeyID(
+            cryptographicSignature: eudcc.cryptographicSignature
+        ) else {
+            // Otherwise complete with failure
+            return .failure(.malformedCertificateKeyID(eudcc))
+        }
+        // Retrieve matching TrustCertificates by KeyID
+        let matchingTrustCertificates = trustCertificates.filter { $0.keyID == eudccKeyID }
+        // Verify matching TrustCertificates are not empty
+        guard !matchingTrustCertificates.isEmpty else {
+            // Otherwise complete with failure
+            return .failure(.noMatchingTrustCertificate(eudccKeyID))
+        }
+        // Retrieve EUDCC Signature bytes
+        let eudccSignature = Data(eudcc.cryptographicSignature.signature)
+        // Initialize EUDCC SignedPayload
+        let eudccSignedPayload = EUDCC.SignedPayload(cryptographicSignature: eudcc.cryptographicSignature)
+        // Map matching SignerCertificates to VerificationCandidates
+        let verificationCandidates: [EUDCC.VerificationCandidate] = matchingTrustCertificates
+            .map { trustCertificate in
+                .init(
+                    signature: eudccSignature,
+                    signedPayload: eudccSignedPayload,
+                    trustCertificate: trustCertificate
+                )
+            }
+        // Verify the first VerificationCandidate that is verified successfully is available
+        guard let matchingVerificationCandidate = verificationCandidates.first(where: { $0.verify() }) else {
+            // Otherwise complete with failure as no VerificationCandidate verified successfully
+            return .invalid
+        }
+        // Complete with success
+        return .success(matchingVerificationCandidate.trustCertificate)
     }
     
 }
